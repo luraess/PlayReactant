dca75ea9112f5bb90bbfb6174fbf9b27b93879d8
Ludovic Raess <ludovic.rass@gmail.com>
2026-04-28 10:40:11 +0200

    Up

--- egu26/diff2D_flux.jl
+++ egu26/diff2D_flux.jl
@@ -122,7 +122,7 @@ function runme(; nx=64, ny=64, nt=10, dtype=Float64, use_cuda::Bool=CUDA.functio
     end
 
     # --- report ---
-    A_eff = 2 * (sizeof(H_ka) + sizeof(qx_ka) + sizeof(qy_ka)) * 1e-9 * nt
+    A_eff = (3 * sizeof(H_ka) + 2 * (sizeof(qx_ka) + sizeof(qy_ka))) * 1e-9 * nt
     println("\n--- Benchmark (nx=$nx, ny=$ny, nt=$nt) ---")
     println("KA plain       time loop: Teff = $(round(A_eff / bm_ka.time,  digits=2)) GB/s")
     println("Reactant KA    time loop: Teff = $(round(A_eff / bm_r.time,   digits=2)) GB/s")
@@ -131,6 +131,6 @@ function runme(; nx=64, ny=64, nt=10, dtype=Float64, use_cuda::Bool=CUDA.functio
     return
 end
 
-res = 16 * 1024
+res = 32 * 1024
 # runme(; nx=res, ny=res, use_cuda=false)
 runme(; nx=res, ny=res, nt=10, use_cuda=true)
